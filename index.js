0d38c27b29b13791760b276cf9267eb5b1c5042e
fishineyuan <382184760@qq.com>
2016-07-15 10:11:06 +0800

    global math all

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 
 'use strict';
 
+var script = /<script(\s+type=["']text\/javascript["'])?>([\s\S]*?)<\/script>/g,
+    style = /<style(\s+type=["']text\/css["'])?>([\s\S]*?)<\/style>/g;
+
 module.exports = function(opts, modified, total, next) {
     modified.forEach(function(file) {
         if (file.isHtmlLike) {
             var content = file.getContent();
 
-            content = content.replace(/(<script(\s+type=["']text\/javascript["'])?>([\s\S]*?)<\/script>\s*){2,}/, function(str) {
-                var single = /<script(\s+type=["']text\/javascript["'])?>([\s\S]*?)<\/script>/g,
-                    code = [],
+            content = content.replace(new RegExp('(' + script.source + '\s*){2,}', 'g'), function(str) {
+                var code = [],
                     match = null;
-                while (match = single.exec(str)) {
+                while (match = script.exec(str)) {
                     code.push(match[2]);
                 }
                 return '<script>' + code.join(';\n') + '</script>\n';
             });
 
-            content = content.replace(/(<style(\s+type=["']text\/css["'])?>([\s\S]*?)<\/style>\s*){2,}/, function(str) {
-                var single = /<style(\s+type=["']text\/css["'])?>([\s\S]*?)<\/style>/g,
-                    code = [],
+            content = content.replace(new RegExp('(' + style.source + '\s*){2,}', 'g'), function(str) {
+                var code = [],
                     match = null;
-                while (match = single.exec(str)) {
+                while (match = style.exec(str)) {
                     code.push(match[2]);
                 }
                 return '<style>' + code.join('\n') + '</style>\n';
